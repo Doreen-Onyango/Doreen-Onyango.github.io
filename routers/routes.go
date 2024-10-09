d0a46de343449febb9631232c545c567cc8e33a8
doonyango <datieno001@gmail.com>
2024-10-09 15:46:24 +0300

    Feat(routes): Add/ Update Route Checker

--- routers/routes.go
+++ routers/routes.go
@@ -8,6 +8,10 @@ import (
 	"github.com/Doreen-Onyango/portfolio.git/utils"
 )
 
+var rts = map[string]bool{
+	"/": true,
+}
+
 func Routes(mux *http.ServeMux) {
 	dir := utils.GetProjectRoot("sections", "static")
 	fServer := http.FileServer(http.Dir(dir))
@@ -17,12 +21,18 @@ func Routes(mux *http.ServeMux) {
 		controllers.Welcomecontroller(w, r)
 	})
 }
+
 func RouteChecker(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if strings.HasPrefix(r.URL.Path, "/static/") {
 			next.ServeHTTP(w, r)
+			return
+		}
+
+		if _, ok := rts[r.URL.Path]; !ok {
+			controllers.NotFoundController(w, r)
+			return
 		}
 		next.ServeHTTP(w, r)
 	})
-
 }
