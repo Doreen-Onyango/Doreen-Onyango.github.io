bb4cce758a2098e91ebd0497fd2e1542b22278c6
doonyango <datieno001@gmail.com>
2024-10-09 15:48:47 +0300

    Feat(controllers): Implement Not found controller and Update welcome page route

--- controllers/route_controller.go
+++ controllers/route_controller.go
@@ -7,5 +7,9 @@ import (
 )
 
 func Welcomecontroller(w http.ResponseWriter, r *http.Request) {
-	utils.RenderTemplate(w, "welcome_page.html", nil)
+	utils.RenderTemplate(w, "index.page.html", nil)
+}
+
+func NotFoundController(w http.ResponseWriter, r *http.Request) {
+	utils.RenderTemplate(w, "404.page.html", nil)
 }
