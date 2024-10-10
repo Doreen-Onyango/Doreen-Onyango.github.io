8e7a23b89b95658d2e6599f4fe30c72c4ebad487
doonyango <datieno001@gmail.com>
2024-10-10 15:43:11 +0300

    feat(controllers): add page controllers

--- controllers/route_controller.go
+++ controllers/route_controller.go
@@ -13,3 +13,15 @@ func Welcomecontroller(w http.ResponseWriter, r *http.Request) {
 func NotFoundController(w http.ResponseWriter, r *http.Request) {
 	utils.RenderTemplate(w, "404.page.html", nil)
 }
+
+func PortfolioController(w http.ResponseWriter, r *http.Request) {
+	utils.RenderTemplate(w, "portfolio.page.html", nil)
+}
+
+func SkillController(w http.ResponseWriter, r *http.Request) {
+	utils.RenderTemplate(w, "skills.page.html", nil)
+}
+
+func ContactsController(w http.ResponseWriter, r *http.Request) {
+	utils.RenderTemplate(w, "contacts.page.html", nil)
+}
