5acbdaf1e902aeb6f1d5599508340fca37300765
doonyango <datieno001@gmail.com>
2024-10-23 22:37:31 +0300

    refacor: update script

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ document.addEventListener("DOMContentLoaded", (event) => {
   contactForm.addEventListener("submit", (e) => {
     // e.preventDefault();
     // Here you would typically send the form data to a server
-    alert("Thank you for your message! I'll get back to you soon.");
-    contactForm.reset();
+    // alert("Thank you for your message! I'll get back to you soon.");
+    // contactForm.reset();
   });
 
   // Easter egg: Konami code
